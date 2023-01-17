c698b2202303186d427cbac5c225bc86b797ebcd
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2023-01-17 11:19:53 -0300

    feat: add post and get tests

--- src/__tests__/integration/sistems/sistem.spec.ts
+++ src/__tests__/integration/sistems/sistem.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import AppDataSource from "../../../data-source";
 import request from "supertest";
 import app from "../../../app";
+import { includeSistemMock, searchSistemMock } from "../../mocks";
 
 describe("/posts", () => {
     let connection: DataSource;
@@ -20,4 +21,28 @@ describe("/posts", () => {
     afterAll(async () => {
       await connection.destroy();
     })
+
+
+    test("POST /sistems - Must be able to include post", async () => {
+        
+        const response = await request(app).post("/sistems").send(includeSistemMock);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("message");
+    });
+
+    test("GET /sistems - Get sistems by body params", async () => {
+        
+        const response = await request(app).get("/sistems").send(searchSistemMock);
+
+        expect(response.body).toHaveLength(1);
+    });
+
+    test("GET /sistems - Not found sistems", async () => {
+        const response =  await request(app).get("/sistems").send(searchSistemMock);
+
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty("message");
+    });
+
 });
---
+++ src/__tests__/mocks/index.ts
@@ -0,0 +1,12 @@
+import { IRequestSistem } from "../../interfaces/sistem"
+
+export const includeSistemMock: IRequestSistem = {
+    description: "Banco de experiências de conselhos escolares",
+    acronym: "BECE"
+}
+
+export const searchSistemMock: IRequestSistem = {
+    description: "",
+    acronym: "",
+    sistemEmail: ""
+}
